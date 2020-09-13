91f1abec705a87e5b00a9bbe836d7148a9e55b4d
Julieta Cash <julietacash@gmail.com>
2020-09-14 00:52:03 +0200

    Added some comments to js and html

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 function playSounds(e) {
-  //e stands for the "event", meaning each time user press any key
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio) return; //This if stops the function if the user imputs a key that hasen't got an audio
+  //e stands for the "event", meaning each time user press any key. Each key contains an object, with an attribute called "keyCode", with a specific number. Those numbers are the value for the tags attribute called "data-key".
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //Selects the audio that matchs the pressed key
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // Selects the button that matchs the pressed key
+  if (!audio) return; //This "if" stops the function if the user imputs a key that hasn't got an audio
   audio.currentTime = 0; //Rewinds audio to start each time user press key
   audio.play();
-  key.classList.add(`playing`);
+  key.classList.add(`playing`); //Adds class playing, that has a different css decoration
 }
 
+//Removes the playing class after the event has "ended"
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove("playing");
 }
 
+// "transitionend" listens to the ending of the events, to call the above function.
 const keys = document.querySelectorAll(`.key`);
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
-window.addEventListener("keydown", playSounds); // Listens everytime user press any key
+// Listens everytime user press any key
+window.addEventListener("keydown", playSounds);

84ecabe8bf8a392ebebbf8c17451475f07a0c8b1
Julieta Cash <julietacash@gmail.com>
2020-09-14 00:38:15 +0200

    Animated the keys with js file

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-window.addEventListener("keydown", function (e) {
-  console.log(e);
-});
+function playSounds(e) {
+  //e stands for the "event", meaning each time user press any key
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //This if stops the function if the user imputs a key that hasen't got an audio
+  audio.currentTime = 0; //Rewinds audio to start each time user press key
+  audio.play();
+  key.classList.add(`playing`);
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(`.key`);
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSounds); // Listens everytime user press any key
